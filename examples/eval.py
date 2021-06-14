4d80be22dfe3c7db714550b8c7e1a4b27b59a91b
Russell J. Hewett <rjh@rjh.io>
2021-06-14 13:06:27 -0400

    Added simple example

---
+++ examples/eval.py
@@ -0,0 +1,18 @@
+# Where did this code come from?
+
+import torch
+
+SMOOTH = 1e-6
+
+def iou(outputs: torch.Tensor, labels: torch.Tensor):
+
+    outputs = outputs.squeeze(1)  # BATCH x 1 x H x W => BATCH x H x W
+
+    intersection = (outputs & labels).float().sum((1, 2))  # Will be zero if Truth=0 or Prediction=0
+    union = (outputs | labels).float().sum((1, 2))         # Will be zero if both are 0
+
+    iou = (intersection + SMOOTH) / (union + SMOOTH)  # We smooth our devision to avoid 0/0
+
+    thresholded = torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10  # This is equal to comparing with thresolds
+
+    return thresholded.mean()  # Or thresholded.mean() if you are interested in average across the batch
