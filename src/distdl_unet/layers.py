3c0497be36d51b0b9c6bc45677857dafb625c9ab
Russell J. Hewett <rjh@rjh.io>
2021-06-14 09:46:51 -0400

    Started turning into a package

---
+++ src/distdl_unet/layers.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn
+
+class Concatenate(torch.nn.Module):
+
+    def __init__(self, axis):
+        super(Concatenate, self).__init__()
+
+        self.axis = axis
+
+    def forward(self, *args):
+
+        return torch.cat(args, self.axis)
---
+++ src/distdl_unet/output.py
@@ -0,0 +1,40 @@
+import torch
+
+import distdl
+from distdl.utilities.torch import zero_volume_tensor
+
+
+class DistributedNetworkOutputFunction(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, input, partition):
+
+        ctx.partition = partition
+        ctx.sh = input.shape
+
+        if partition.rank == 0:
+            return input.clone()
+        else:
+            return torch.tensor([0.0], requires_grad=True).float()
+
+    @staticmethod
+    def backward(ctx, grad_output):
+
+        partition = ctx.partition
+        sh = ctx.sh
+
+        if partition.rank == 0:
+            return grad_output.clone(), None
+        else:
+            return zero_volume_tensor(sh[0]), None
+
+
+class DistributedNetworkOutput(distdl.nn.Module):
+
+    def __init__(self, partition):
+        super(DistributedNetworkOutput, self).__init__()
+        self.partition = partition
+
+    def forward(self, input):
+
+        return DistributedNetworkOutputFunction.apply(input, self.partition)
